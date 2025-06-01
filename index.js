eb6dd99afe34776a2c1c5526b241b3b5413da8ab
teddy-ctrl <tedu21@gmail.com>
2025-06-01 11:31:00 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+
+app.use(cors({ origin: true }));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Success !",
+  });
+});
+
+app.post("/payment/create", async (req, res) => {
+  const total = req.query.total;
+
+  if (total > 0) {
+    // console.log("payment recieved", total);
+    // res.send(total)
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount:total,
+        currency: 'USD',
+    })
+    console.log(paymentIntent);
+
+    res.status(201).json({
+        clientSecret: paymentIntent.client_secret,
+    });
+  } else {
+        res.status(403).json({
+            message: 'total must be greater than 0'
+        })
+  }
+});
+
+
+app.listen(5000, (err) => {
+    if (err) throw err;
+    console.log('Amazon Server Running on PORT: 5000, http://localhost:5000');
+});
+
